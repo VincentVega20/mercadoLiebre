7f163ec0bfe49ecd6adfda199844a3648654547b
VincentVega20 <maximolopardo@gmail.com>
2024-02-26 23:49:26 -0300

    agregado todo MercadoLiebre

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const publicPath = path.resolve(__dirname, "./public")
+const port = 3000
+
+app.listen(port, () => 
+console.log("servidor corriendo en puerto " + port))
+
+app.use(express.static(publicPath));
+
+app.get("/", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/register", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.post("/register", function(req, res){
+    res.send("Se registró con exito")
+})
+
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.post("/login", function(req, res){
+    res.send("Bienvenido")
+})
