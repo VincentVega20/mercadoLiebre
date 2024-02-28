148b0242bec0294b5b960089892262a68f1f402c
VincentVega20 <maximolopardo@gmail.com>
2024-02-27 21:44:34 -0300

    agregado ruteo

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
-const port = 3000
+const port = process.env.PORT || 3000 /* Inicializo PORT en render o servidor 3000 */
+
 
 app.listen(port, () => 
 console.log("servidor corriendo en puerto " + port))
